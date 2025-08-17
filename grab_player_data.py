079aabed07f7053a4178111389c5e6f1a58c17d8
leoplute <leoplute@gmail.com>
2025-08-17 13:56:48 -0600

    Upgraded the way I grab player stats to optimize the API calls and it dramatically sped up the time of the program, now I can probably switch the loading screen up a bit.

--- grab_player_data.py
+++ grab_player_data.py
@@ -5,142 +5,121 @@ ADVANCED_URL = "https://api.server.nbaapi.com/api/playeradvancedstats"
 
 class grabPlayerData:
 
-    # Returns a list of potential player IDs with the first 5 digits of last name + first 2 digits 
-    # of first name, adding 01,02,03,04, and 05 to the end and hoping one of those is the correct player.
-    # Handles if multiple players have similar ID in get_player_stats function
-    def get_potential_player_ids(self, player_name):
+    def buildPlayerIdStart(self, player_name):
         names = player_name.split(" ")
         first_name = names[0]
         last_name = names[1]
-        
-        if len(last_name) > 5:
-            last_name_id = last_name[0:5]
-        else:
-            last_name_id = last_name
 
         if len(first_name) > 2:
             first_name_id = first_name[0:2]
         else:
             first_name_id = first_name
 
+        if len(last_name) > 5:
+            last_name_id = last_name[0:5]
+        else:
+            last_name_id = last_name
+
         id = last_name_id.lower() + first_name_id.lower()
-
-        potential_ids = []
-        potential_ids.append(id + '01')
-        potential_ids.append(id + '02')
-        potential_ids.append(id + '03')
-        potential_ids.append(id + '04')
-        potential_ids.append(id + '05')
-
-        return potential_ids
+        return id
 
     def get_player_stats(self, player_name, season=2025):
         headers = {"accept": "application/json"}
 
-        potential_player_ids = self.get_potential_player_ids(player_name)
-
         # These store all potential matches
-        potential_player_stats = {}
-        potential_player_advanced_stats = {}
+        player_stats = {}
+        advanced_stats = {}
 
-        for playerId in potential_player_ids:
+        looking_for_player = True
+        iteration = 1
+
+        playerId = ''
+
+        while looking_for_player:
+            startingId = self.buildPlayerIdStart(player_name)
+            id = f"{startingId}0{iteration}"
 
             params = {
                 "page": 1,
                 "pageSize": 1000,
                 "season": season,
-                "playerId": playerId,
+                "playerId": id,
             }
-            
-            # Get the season totals
+
             response = requests.get(BASE_URL, params=params, headers=headers)
             if response.status_code == 200:
                 data = response.json()
-                players_list = data.get("data", [])
-                for player in players_list:
-                    potential_player_stats[player['playerName']] = player
+                player_data_list = data.get('data', [])
+                if player_data_list:
+                    player_data = player_data_list[0]  # Will get 2 responses for 1 player, first is regular season, second is playoffs
+                    if player_data['playerName'] == player_name:
+                        player_stats = player_data
+                        playerId = id
+                        looking_for_player = False
+                    else:
+                        iteration += 1
+                else:
+                    iteration += 1
             else:
-                print(f'No player matches the playerId of: {playerId}')
+                iteration += 1
 
-            # Get the advanced stats
-            advancedResponse = requests.get(ADVANCED_URL, params=params, headers=headers)
-            if advancedResponse.status_code == 200:
-                data = advancedResponse.json()
-                advanced_player_list = data.get("data", [])
-                for player in advanced_player_list:
-                    potential_player_advanced_stats[player['playerName']] = player
-            else:
-                print(f"No advanced stats found for playerId: {playerId}")
+        # Player data 
+        params = {
+                "page": 1,
+                "pageSize": 1000,
+                "season": season,
+                "playerId": playerId,
+            }
+        advanced_response = requests.get(ADVANCED_URL, params=params, headers=headers)
+        if advanced_response.status_code == 200:
+            advanced_data = advanced_response.json()
+            player_advanced_data_list = advanced_data.get('data', [])
+            if player_advanced_data_list:
+                player_advanced_data = player_advanced_data_list[0]
+                advanced_stats = player_advanced_data
 
-
-        # If there are no matches
-        if not potential_player_stats:
-            print(f"No stats found for any potential ID for the player {player_name}")
-            return {}
-        
-        # If there is exactly one match (the correct player)
-        if len(potential_player_stats) == 1:
-            player_name_key = list(potential_player_stats.keys())[0]
-            playerStats = potential_player_stats[player_name_key]
-        
-        # If there is more than one match, find the correct player by name
-        else:
-            playerStats = None
-            player_name_key = None
-            for pname, player in potential_player_stats.items():
-                if pname.lower() == player_name.lower():
-                    playerStats = player
-                    player_name_key = pname
-                    break
-            if not playerStats:
-                print('issue finding player based on the id')
-                return {}
-            
-        advanced_stats = potential_player_advanced_stats.get(player_name_key, {})
-        if not advanced_stats:
-            print(f"Warning: No advanced stats found for {player_name_key}")
 
         relevant_stats = {
-                    "position": playerStats['position'],
-                    "minutespg": playerStats['minutesPg'],
-                    "fg%": playerStats['fieldPercent'],
-                    "3%": playerStats['threePercent'],
-                    "ft%": playerStats['ftPercent'],
-                    "efg%": playerStats['effectFgPercent'],
-                    "2attempts": playerStats['twoAttempts'],
-                    "2made": playerStats['twoFg'],
-                    "3attempts": playerStats['threeAttempts'],
-                    "3made": playerStats['threeFg'],
-                    "ftAttempts": playerStats['ftAttempts'],
-                    "ft": playerStats['ft'],
-                    "points": playerStats['points'],
-                    "rebounds": playerStats['totalRb'],
-                    "offensiveRebounds": playerStats['offensiveRb'],
-                    "defensiveRebounds": playerStats['defensiveRb'],
-                    "assists": playerStats['assists'],
-                    "steals": playerStats['steals'],
-                    "blocks": playerStats['blocks'],
-                    "turnovers": playerStats['turnovers'],
-                    "fouls": playerStats['personalFouls'],
-                    "games": playerStats['games'],
+            "position": player_stats['position'],
+            "minutespg": player_stats['minutesPg'],
+            "fg%": player_stats['fieldPercent'],
+            "3%": player_stats['threePercent'],
+            "ft%": player_stats['ftPercent'],
+            "efg%": player_stats['effectFgPercent'],
+            "2attempts": player_stats['twoAttempts'],
+            "2made": player_stats['twoFg'],
+            "3attempts": player_stats['threeAttempts'],
+            "3made": player_stats['threeFg'],
+            "ftAttempts": player_stats['ftAttempts'],
+            "ft": player_stats['ft'],
+            "points": player_stats['points'],
+            "rebounds": player_stats['totalRb'],
+            "offensiveRebounds": player_stats['offensiveRb'],
+            "defensiveRebounds": player_stats['defensiveRb'],
+            "assists": player_stats['assists'],
+            "steals": player_stats['steals'],
+            "blocks": player_stats['blocks'],
+            "turnovers": player_stats['turnovers'],
+            "fouls": player_stats['personalFouls'],
+            "games": player_stats['games'],
         }
 
         if advanced_stats:
-            relevant_stats.update({
-                "total_minutes": advanced_stats['minutesPlayed'],
-                "ts%": advanced_stats['tsPercent'],
-                "rebound%": advanced_stats['totalRBPercent'],
-                "offensiveRbPercent": advanced_stats['offensiveRBPercent'],
-                "defensiveRbPercent": advanced_stats['defensiveRBPercent'],
-                "usage%": advanced_stats['usagePercent'],
-                "offensePlusMinus": advanced_stats['offensiveBox'],
-                "defensivePlusMinus": advanced_stats['defensiveBox'],
-                "plusMinus": advanced_stats['box'],
-                "steal%": advanced_stats['stealPercent'],
-                "block%": advanced_stats['blockPercent'],
-                "turnover%": advanced_stats['turnoverPercent'],
-            })
-        
+                relevant_stats.update({
+                    "total_minutes": advanced_stats['minutesPlayed'],
+                    "ts%": advanced_stats['tsPercent'],
+                    "rebound%": advanced_stats['totalRBPercent'],
+                    "offensiveRbPercent": advanced_stats['offensiveRBPercent'],
+                    "defensiveRbPercent": advanced_stats['defensiveRBPercent'],
+                    "usage%": advanced_stats['usagePercent'],
+                    "offensePlusMinus": advanced_stats['offensiveBox'],
+                    "defensivePlusMinus": advanced_stats['defensiveBox'],
+                    "plusMinus": advanced_stats['box'],
+                    "steal%": advanced_stats['stealPercent'],
+                    "block%": advanced_stats['blockPercent'],
+                    "turnover%": advanced_stats['turnoverPercent'],
+                })
+
         return relevant_stats
 
 
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-import pytest
 import requests
+
+BASE_URL = "https://api.server.nbaapi.com/api/playertotals"
+ADVANCED_URL = "https://api.server.nbaapi.com/api/playeradvancedstats"
+
 
 def find_player_id(player_name, season=2025):
     """Find player ID by searching all players in the season"""
@@ -36,7 +39,7 @@ def test_find_specific_player():
     """Test to find a specific player's ID - CHANGE THE NAME HERE"""
     
     # *** CHANGE THIS PLAYER NAME TO WHOEVER YOU WANT TO TEST ***
-    player_name = "Jalen Williams"
+    player_name = "Kevin Durant"
     
     player_id = find_player_id(player_name)
     
@@ -46,5 +49,126 @@ def test_find_specific_player():
     assert player_id is not None, f"Could not find player ID for {player_name}"
 
 
+###################################################################################################33
+
+def buildPlayerIdStart(player_name):
+        names = player_name.split(" ")
+        first_name = names[0]
+        last_name = names[1]
+
+        if len(first_name) > 2:
+            first_name_id = first_name[0:2]
+        else:
+            first_name_id = first_name
+
+        if len(last_name) > 5:
+            last_name_id = last_name[0:5]
+        else:
+            last_name_id = last_name
+
+        id = last_name_id.lower() + first_name_id.lower()
+        return id
+
+def get_player_stats_test(player_name, season=2025):
+    headers = {"accept": "application/json"}
+
+    # These store all potential matches
+    player_stats = {}
+    advanced_stats = {}
+
+    looking_for_player = True
+    iteration = 1
+
+    playerId = ''
+
+    while looking_for_player:
+        startingId = buildPlayerIdStart(player_name)
+        id = f"{startingId}0{iteration}"
+
+        params = {
+            "page": 1,
+            "pageSize": 1000,
+            "season": season,
+            "playerId": id,
+        }
+
+        response = requests.get(BASE_URL, params=params, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            player_data_list = data.get('data', [])
+            if player_data_list:
+                player_data = player_data_list[0]  # Will get 2 responses for 1 player, first is regular season, second is playoffs
+                if player_data['playerName'] == player_name:
+                    player_stats = player_data
+                    playerId = id
+                    looking_for_player = False
+                else:
+                    iteration += 1
+            else:
+                iteration += 1
+        else:
+            iteration += 1
+
+    # Player data 
+    params = {
+            "page": 1,
+            "pageSize": 1000,
+            "season": season,
+            "playerId": playerId,
+        }
+    advanced_response = requests.get(ADVANCED_URL, params=params, headers=headers)
+    if advanced_response.status_code == 200:
+        advanced_data = advanced_response.json()
+        player_advanced_data_list = advanced_data.get('data', [])
+        if player_advanced_data_list:
+            player_advanced_data = player_advanced_data_list[0]
+            advanced_stats = player_advanced_data
+
+
+    relevant_stats = {
+        "position": player_stats['position'],
+        "minutespg": player_stats['minutesPg'],
+        "fg%": player_stats['fieldPercent'],
+        "3%": player_stats['threePercent'],
+        "ft%": player_stats['ftPercent'],
+        "efg%": player_stats['effectFgPercent'],
+        "2attempts": player_stats['twoAttempts'],
+        "2made": player_stats['twoFg'],
+        "3attempts": player_stats['threeAttempts'],
+        "3made": player_stats['threeFg'],
+        "ftAttempts": player_stats['ftAttempts'],
+        "ft": player_stats['ft'],
+        "points": player_stats['points'],
+        "rebounds": player_stats['totalRb'],
+        "offensiveRebounds": player_stats['offensiveRb'],
+        "defensiveRebounds": player_stats['defensiveRb'],
+        "assists": player_stats['assists'],
+        "steals": player_stats['steals'],
+        "blocks": player_stats['blocks'],
+        "turnovers": player_stats['turnovers'],
+        "fouls": player_stats['personalFouls'],
+        "games": player_stats['games'],
+    }
+
+    if advanced_stats:
+            relevant_stats.update({
+                "total_minutes": advanced_stats['minutesPlayed'],
+                "ts%": advanced_stats['tsPercent'],
+                "rebound%": advanced_stats['totalRBPercent'],
+                "offensiveRbPercent": advanced_stats['offensiveRBPercent'],
+                "defensiveRbPercent": advanced_stats['defensiveRBPercent'],
+                "usage%": advanced_stats['usagePercent'],
+                "offensePlusMinus": advanced_stats['offensiveBox'],
+                "defensivePlusMinus": advanced_stats['defensiveBox'],
+                "plusMinus": advanced_stats['box'],
+                "steal%": advanced_stats['stealPercent'],
+                "block%": advanced_stats['blockPercent'],
+                "turnover%": advanced_stats['turnoverPercent'],
+            })
+
+    return relevant_stats
+
+
 if __name__ == "__main__":
-    test_find_specific_player()
+    #test_find_specific_player()
+    print(get_player_stats_test('LeBron James'))
